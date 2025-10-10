e8a68411caa78f801c3987d03ec24905ed2b8906
root <root@ip-172-31-43-124.eu-west-3.compute.internal>
2025-10-10 19:25:41 +0000

    updates

--- config/config/bundles.php
+++ config/config/bundles.php
@@ -1,7 +1,6 @@
 <?php
 
-return [    
-    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
