fa202636c04e1d9c645906bf8de136520d4f270c
hedilarbi <hedi.larbi@esprit.tn>
2025-11-19 23:32:07 +0100

    hhuho

--- src/Services/PrayTimesService.php
+++ src/Services/PrayTimesService.php
@@ -8,7 +8,7 @@ use Doctrine\ORM\EntityManagerInterface;
 class PrayTimesService
 {
     public function __construct(private readonly EntityManagerInterface $entityManager) {
-        $this->PrayTime(7);
+        $this->PrayTime(6);
     }
 
 
@@ -93,7 +93,7 @@ class PrayTimesService
 
 
 public function getUserPrayTimes($userLocation, $timestampday) {
-    $this->setCalcMethod(7); 
+    $this->setCalcMethod(6); 
 
   
     $tz = new \DateTimeZone('Etc/GMT-1');
