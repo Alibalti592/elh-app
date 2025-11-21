a6e6654565c5869266755bba1458cc2a2b822d05
hedilarbi <hedi.larbi@esprit.tn>
2025-11-21 10:45:54 +0100

    qweqwfqefqw

--- src/Controller/Api/UserController.php
+++ src/Controller/Api/UserController.php
@@ -44,7 +44,7 @@ class UserController extends AbstractController
         $userRegistration = $data->userRegistration;
         $jsonReponse = new JsonResponse();
         $alreadyExist = $this->entityManager->getRepository(User::class)->findOneBy(['email' => $userRegistration->email]);
-        $phoneAlreadyExist = $this->entityManager->getRepository(User::class)->findOneBy(['phone' => $userRegistration->phone]);
+      
         if(!is_null($alreadyExist)) {
             $jsonReponse->setData([
                 'message' => 'Ce compte existe déjà !'
@@ -52,13 +52,7 @@ class UserController extends AbstractController
             $jsonReponse->setStatusCode(409);
             return $jsonReponse;
         }
-        if(!is_null($phoneAlreadyExist)) {
-            $jsonReponse->setData([
-                'message' => 'Ce numéro de téléphone est déjà utilisé pour un autre compte !'
-            ]);
-            $jsonReponse->setStatusCode(409);
-            return $jsonReponse;
-        }
+       
         try {
             $user = new User();
             $user->setEmail($userRegistration->email);
