d2b7950c44c1f3cd28afd1da04bf7542c69f0114
hedi larbi <hedilarbi@hedis-MacBook-Air.local>
2026-03-08 17:16:16 +0100

    12

--- src/Entity/NotifToSend.php
+++ src/Entity/NotifToSend.php
@@ -76,10 +76,7 @@ class NotifToSend
         $sendAt->setTimestamp($timestamp);
         $this->setUser($currentUser);
         $prayKey = (string) ($praytimeUI['key'] ?? '');
-        if ($prayKey === 'fajr') {
-            $this->setTitle("🔔 Farj : Plus que 15 minutes pour la  prière🕌");
-            $this->setMessage("Le réveil est dur, mais la bénédiction est tellement grande. Allez, motive-toi ! 🤍");
-        } elseif ($prayKey === 'chorouq') {
+        if ($prayKey === 'chorouq') {
             $this->setTitle("🔔 Plus que 15 minutes pour chorouq");
             $this->setMessage("Le réveil est dur, mais la bénédiction est tellement grande. Allez, motive-toi ! 🤍");
         } else {
